da51c2d559eaadd9e69fac68cebe4368eed39184
AndreyLushchik <redbull199556@gmail.com>
2021-11-26 23:54:29 +0300

    create cicle app with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function UserList() {
+	return (
+		<div>
+			{data.map((obj) => {
+				return (
+					<div key={`${obj._id}`}>
+						<h3>{obj.name.first}</h3>
+						<h4>{obj.name.last}</h4>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
-export default App;
+export default UserList;
